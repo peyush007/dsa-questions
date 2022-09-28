19030b1b33e2c44d225e6455caceeb2ae2abfb52
s4ms3p1ol <tonystark8739@gmail.com>
2022-09-29 00:38:54 +0530

    dsa questions

---
+++ minand2ndmin.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> minAnd2ndMin(int a[], int n);
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+
+        for (int i = 0; i < n; i++) 
+            cin >> a[i];
+
+        vector<int> ans = minAnd2ndMin(a, n);
+        if (ans[0] == -1)
+            cout << -1 << endl;
+        else 
+            cout << ans[0] << " " << ans[1] << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+vector<int> minAnd2ndMin(int a[], int n) {
+    vector<int> v;
+    sort(a,a+n);
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]!=a[i+1])
+        {
+            v.push_back(a[i]);
+        }
+    }
+    vector<int> k;
+    if(v.size()<2)
+    {
+        k.push_back(-1);
+        return k;
+    }
+    k.push_back(v[0]);
+    k.push_back(v[1]);
+    return k;
+    
+}
