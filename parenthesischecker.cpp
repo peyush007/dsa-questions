36509331c177c8c3fad78e8c186f7a264b4b4807
s4ms3p1ol <tonystark8739@gmail.com>
2022-09-29 21:56:19 +0530

    dsa questions

---
+++ parenthesischecker.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        // Your code here
+         stack<char> s;
+     for(int i=0; i<x.length(); i++) {
+         
+         char ch = x[i];
+         
+         //if opening bracket, stack push
+         //if close bracket, stacktop check and pop
+         
+         if(ch == '(' || ch == '{' || ch == '['){
+             s.push(ch);
+         }
+         else
+         {
+             //for closing bracket
+             if(!s.empty()) {
+                  char top = s.top();
+                  if( (ch == ')' && top == '(') || 
+                     ( ch == '}' && top == '{') || 
+                     (ch == ']' && top == '[') ) {
+                      s.pop();
+                  }
+                 else
+                 {
+                     return false;
+                 }
+             }
+             else
+             {
+                 return false;
+             } 
+         }  
+     }
+    
+    if(s.empty())
+        return true;
+    else
+        return false;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   int t;
+   string a;
+   cin>>t;
+   while(t--)
+   {
+       cin>>a;
+       Solution obj;
+       if(obj.ispar(a))
+        cout<<"balanced"<<endl;
+       else
+        cout<<"not balanced"<<endl;
+   }
+}
+// } Driver Code Ends
---
+++ sort012.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        // code here 
+        int j,k,l;
+        j=0;
+        k=0;
+        l=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==0)
+            j++;
+            if(a[i]==1)
+            k++;
+            else
+            l++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(j>0)
+            {
+                a[i]=0;
+                j--;
+            }
+            else if(k>0)
+            {
+                a[i] = 1;
+                k--;
+            }
+            else
+            a[i] = 2;
+                
+        }
+            
+            
+           
+            
+            
+        
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
